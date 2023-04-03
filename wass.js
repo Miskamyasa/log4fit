c90d25b47abfa2030c5b68919db9abea09aa089c
Denis Zaitsev <zaytsevdi@gmail.com>
2023-04-03 23:17:22 +0300

    ...

--- wass.js
+++ wass.js
@@ -2,42 +2,42 @@ const {withAndroidManifest} = require("@expo/config-plugins")
 
 
 const attrs = {
-  "resizeable": false,
-  "smallScreens": false,
-  "normalScreens": true,
-  "largeScreens": true,
-  "xlargeScreens": false,
-  "anyDensity": true,
-  "requiresSmallestWidthDp": 320,
-  "compatibleWidthLimitDp": 480,
-  "largestWidthLimitDp": 575,
+    "resizeable": false,
+    "smallScreens": false,
+    "normalScreens": true,
+    "largeScreens": true,
+    "xlargeScreens": false,
+    "anyDensity": true,
+    "requiresSmallestWidthDp": 320,
+    "compatibleWidthLimitDp": 480,
+    "largestWidthLimitDp": 575,
 }
 
 function namespacedAttributes(attributes) {
-  let namespacedObj = {}
+    let namespacedObj = {}
 
-  for (const [key, value] of Object.entries(attributes)) {
-    namespacedObj[`android:${key}`] = value
-  }
+    for (const [key, value] of Object.entries(attributes)) {
+        namespacedObj[`android:${key}`] = value
+    }
 
-  return namespacedObj
+    return namespacedObj
 }
 
 function addSupportsScreens(androidManifest) {
-  const {manifest} = androidManifest
+    const {manifest} = androidManifest
 
-  manifest["supports-screens"] = [
-    {
-      $: namespacedAttributes(attrs),
-    },
-  ]
+    manifest["supports-screens"] = [
+        {
+            $: namespacedAttributes(attrs),
+        },
+    ]
 
-  return androidManifest
+    return androidManifest
 }
 
 module.exports = function withAndroidSupportsScreens(config) {
-  return withAndroidManifest(config, (config) => {
-    config.modResults = addSupportsScreens(config.modResults)
-    return config
-  })
+    return withAndroidManifest(config, (config) => {
+        config.modResults = addSupportsScreens(config.modResults)
+        return config
+    })
 }
