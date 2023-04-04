544cf4dd0a9eec5968d033904b29f207625f4aae
Denis Zaitsev <zaytsevdi@gmail.com>
2023-04-04 16:55:54 +0300

    ...

--- src/helpers/errorHandler.ts
+++ src/helpers/errorHandler.ts
@@ -1,9 +1,3 @@
-// import {Alert} from "react-native"
-
-// import * as Updates from "expo-updates"
-
-// import {__t} from "../i18"
-
 import analytics from "./analytics"
 
 
@@ -16,15 +10,6 @@ function errorHandler(err: unknown): void {
     typeof err === "object"
         ? console.warn(JSON.stringify({...err}, null, 2))
         : console.warn((err as Error).message)
-
-    // return Alert.alert("Error", "Something wierd is happening", [
-    //   {
-    //     text: __t("reload"),
-    //     onPress: !__DEV__
-    //       ? (): void => void Updates.reloadAsync()
-    //       : (): null => null,
-    //   },
-    // ], {cancelable: false})
 }
 
 export default errorHandler
