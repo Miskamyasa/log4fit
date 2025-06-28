bcb8c3261ffbf0a2a7596f065fb6c9b0e8b7d4b1
Denis Zaitsev <zaytsevdi@gmail.com>
2025-06-28 13:47:14 +0300

    Format and sort ESLint rules alphabetically

--- eslint.config.js
+++ eslint.config.js
@@ -57,195 +57,232 @@ export default defineConfig([
             },
         },
         rules: {
-            "@typescript-eslint/explicit-function-return-type": "error",
-            "@typescript-eslint/no-unused-vars": "error",
-            "@typescript-eslint/no-misused-promises": [
+            "@stylistic/block-spacing": [
+                "error",
+                "never"
+            ],
+            "@stylistic/comma-dangle": [
+                "error",
+                "always-multiline"
+            ],
+            "@stylistic/indent": [
+                "error",
+                4
+            ],
+            "@stylistic/jsx-closing-bracket-location": [
                 "error",
                 {
-                    checksVoidReturn: {
-                        attributes: false,
-                    },
-                },
+                    "nonEmpty": "after-props",
+                    "selfClosing": "after-props"
+                }
             ],
-            "unused-imports/no-unused-imports": "error",
-            "arrow-body-style": "off",
-            "@stylistic/type-annotation-spacing": "error",
-            "@stylistic/space-infix-ops": ["error", {int32Hint: false}],
-            "@stylistic/comma-dangle": ["error", "always-multiline"],
-            "@stylistic/linebreak-style": ["error", "unix"],
+            "@stylistic/jsx-curly-spacing": [
+                "error",
+                {
+                    "attributes": {
+                        "allowMultiline": false
+                    },
+                    "children": true,
+                    "when": "never"
+                }
+            ],
+            "@stylistic/jsx-first-prop-new-line": [
+                "error",
+                "multiline-multiprop"
+            ],
+            "@stylistic/jsx-indent-props": [
+                "error",
+                4
+            ],
+            "@stylistic/jsx-max-props-per-line": [
+                "error",
+                {
+                    "maximum": 1
+                }
+            ],
+            "@stylistic/jsx-sort-props": [
+                "error",
+                {
+                    "callbacksLast": true,
+                    "ignoreCase": true,
+                    "noSortAlphabetically": false,
+                    "reservedFirst": true,
+                    "shorthandFirst": true,
+                    "shorthandLast": false
+                }
+            ],
+            "@stylistic/jsx-tag-spacing": [
+                "error",
+                {
+                    "afterOpening": "never",
+                    "beforeClosing": "allow",
+                    "beforeSelfClosing": "allow",
+                    "closingSlash": "never"
+                }
+            ],
+            "@stylistic/jsx-wrap-multilines": [
+                "error",
+                {
+                    "arrow": "parens",
+                    "assignment": "parens",
+                    "condition": "ignore",
+                    "declaration": "parens",
+                    "logical": "ignore",
+                    "prop": "ignore",
+                    "return": "parens"
+                }
+            ],
+            "@stylistic/key-spacing": [
+                "error",
+                {
+                    "afterColon": true,
+                    "mode": "strict"
+                }
+            ],
+            "@stylistic/keyword-spacing": [
+                "error",
+                {
+                    "after": true,
+                    "before": true
+                }
+            ],
+            "@stylistic/linebreak-style": [
+                "error",
+                "unix"
+            ],
             "@stylistic/max-len": [
                 "warn",
                 {
-                    code: 120,
-                },
-            ],
-            "no-console": [
-                "warn",
-                {
-                    allow: ["warn", "error"],
-                },
+                    "code": 120
+                }
             ],
             "@stylistic/no-multiple-empty-lines": [
                 "error",
                 {
-                    max: 1,
-                    maxEOF: 0,
-                },
+                    "max": 1,
+                    "maxEOF": 0
+                }
             ],
-            "no-use-before-define": "error",
-            "@stylistic/object-curly-spacing": ["error", "never"],
-            "@stylistic/operator-linebreak": ["error", "before"],
+            "@stylistic/object-curly-newline": [
+                "error",
+                {
+                    "consistent": true,
+                    "multiline": true
+                }
+            ],
+            "@stylistic/object-curly-spacing": [
+                "error",
+                "never"
+            ],
+            "@stylistic/object-property-newline": [
+                "error",
+                {
+                    "allowAllPropertiesOnSameLine": true
+                }
+            ],
+            "@stylistic/operator-linebreak": [
+                "error",
+                "before"
+            ],
             "@stylistic/quotes": [
                 "error",
                 "double",
                 {
-                    avoidEscape: true,
-                },
+                    "avoidEscape": true
+                }
             ],
+            "@stylistic/semi": [
+                "error",
+                "never"
+            ],
+            "@stylistic/space-before-blocks": [
+                "error",
+                "always"
+            ],
+            "@stylistic/space-infix-ops": "error",
+            "@stylistic/switch-colon-spacing": [
+                "error",
+                {
+                    "after": true,
+                    "before": false
+                }
+            ],
+            "@stylistic/type-annotation-spacing": "error",
+            "@typescript-eslint/explicit-function-return-type": "error",
+            "@typescript-eslint/no-misused-promises": [
+                "error",
+                {
+                    "checksVoidReturn": {
+                        "attributes": false
+                    }
+                }
+            ],
+            "@typescript-eslint/no-unused-vars": "error",
+            "arrow-body-style": "off",
             "import/newline-after-import": [
                 "error",
                 {
-                    count: 1,
-                },
+                    "count": 1
+                }
             ],
+            "import/no-anonymous-default-export": "error",
+            "import/no-cycle": [
+                "error",
+                {
+                    "ignoreExternal": true,
+                    "maxDepth": 10
+                }
+            ],
+            "import/no-duplicates": "error",
+            "import/no-useless-path-segments": "error",
             "import/order": [
                 "error",
                 {
-                    "pathGroups": [
-                        {
-                            pattern: "react+(|-native)",
-                            group: "external",
-                            position: "before",
-                        },
-                    ],
-                    "pathGroupsExcludedImportTypes": ["react", "react-native"],
+                    "alphabetize": {
+                        "caseInsensitive": true,
+                        "order": "asc"
+                    },
                     "groups": [
                         "builtin",
                         "external",
                         "internal",
                         "parent",
-                        "sibling",
+                        "sibling"
                     ],
                     "newlines-between": "always",
-                    "alphabetize": {
-                        order: "asc",
-                        caseInsensitive: true,
-                    },
-                },
+                    "pathGroups": [
+                        {
+                            "group": "external",
+                            "pattern": "react+(|-native)",
+                            "position": "before"
+                        }
+                    ],
+                    "pathGroupsExcludedImportTypes": [
+                        "react",
+                        "react-native"
+                    ]
+                }
             ],
-            "import/no-cycle": [
-                "error",
+            "no-console": [
+                "warn",
                 {
-                    maxDepth: 10,
-                    ignoreExternal: true,
-                },
+                    "allow": [
+                        "warn",
+                        "error"
+                    ]
+                }
             ],
-            "import/no-useless-path-segments": "error",
-            "import/no-anonymous-default-export": "error",
-            "import/no-duplicates": "error",
-            "@stylistic/jsx-closing-bracket-location": [
+            "no-use-before-define": "error",
+            "react-hooks/exhaustive-deps": "error",
+            "react-native/no-unused-styles": "error",
+            "react-native/no-raw-text": "off",
+            "react/jsx-fragments": [
                 "error",
-                {
-                    nonEmpty: "after-props",
-                    selfClosing: "after-props",
-                },
+                "element"
             ],
             "react/prop-types": "off",
-            "@stylistic/jsx-curly-spacing": [
-                "error",
-                {
-                    when: "never",
-                    attributes: {
-                        allowMultiline: false,
-                    },
-                    children: true,
-                },
-            ],
-            "@stylistic/jsx-first-prop-new-line": [
-                "error",
-                "multiline-multiprop",
-            ],
-            "react-hooks/exhaustive-deps": "error",
-            "react/jsx-fragments": ["error", "element"],
-            "@stylistic/jsx-max-props-per-line": [
-                "error",
-                {
-                    maximum: 1,
-                },
-            ],
-            "@stylistic/jsx-sort-props": [
-                "error",
-                {
-                    callbacksLast: true,
-                    shorthandFirst: true,
-                    shorthandLast: false,
-                    ignoreCase: true,
-                    noSortAlphabetically: false,
-                    reservedFirst: true,
-                },
-            ],
-            "@stylistic/jsx-tag-spacing": [
-                "error",
-                {
-                    closingSlash: "never",
-                    beforeSelfClosing: "allow",
-                    afterOpening: "never",
-                    beforeClosing: "allow",
-                },
-            ],
-            "@stylistic/jsx-wrap-multilines": [
-                "error",
-                {
-                    declaration: "parens",
-                    assignment: "parens",
-                    return: "parens",
-                    arrow: "parens",
-                    condition: "ignore",
-                    logical: "ignore",
-                    prop: "ignore",
-                },
-            ],
             "react/react-in-jsx-scope": "off",
-            "react-native/no-raw-text": "off",
-            "@stylistic/semi": ["error", "never"],
-            "@stylistic/block-spacing": ["error", "never"],
-            "@stylistic/key-spacing": [
-                "error",
-                {
-                    afterColon: true,
-                    mode: "strict",
-                },
-            ],
-            "@stylistic/keyword-spacing": [
-                "error",
-                {
-                    before: true,
-                    after: true,
-                },
-            ],
-            "@stylistic/space-before-blocks": ["error", "always"],
-            "@stylistic/switch-colon-spacing": [
-                "error",
-                {
-                    after: true,
-                    before: false,
-                },
-            ],
-            "@stylistic/object-property-newline": [
-                "error",
-                {
-                    allowAllPropertiesOnSameLine: true,
-                },
-            ],
-            "@stylistic/object-curly-newline": [
-                "error",
-                {
-                    multiline: true,
-                    consistent: true,
-                },
-            ],
-            "@stylistic/jsx-indent-props": ["error", 4],
-            "@stylistic/indent": ["error", 4],
+            "unused-imports/no-unused-imports": "error"
         },
     },
     {
